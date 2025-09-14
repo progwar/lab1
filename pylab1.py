c148076259019bf0a5c4c02968b3f942235529fb
Иван <ivan071006@gmail.com>
2025-09-15 01:25:45 +0300

    6 commit

--- pylab1.py
+++ pylab1.py
@@ -3,3 +3,4 @@ for x in range(0, 2):
     for y in range(0, 2):
         for z in range(0, 2):
             for w in range(0, 2):
+                if not((x == (w or y)) or ((w <= z) and (y <= w))):
